8fd6404766a94cc4bbcc1c730687e2a1b0a09315
bobnearents <bobnearents@yahoo.com>
2019-01-17 12:54:04 -0800

    began add item functionality

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+function generateHtml(item) {
+  return `<li>
+    <span class="shopping-item">${item}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle">
+        <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete">
+        <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`
+}
+
+
+
 function main() {
   //shopping-item__checked
   $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
@@ -5,6 +21,18 @@ function main() {
     targetStrike.toggleClass('shopping-item__checked');
    // .addClass('.shopping-item__checked');
   })
+
+  $('submit').submit(function(event){
+    event.preventDefault();
+  //   $('.shopping-list').append(generateHtml($('#shopping-list-entry').submit(event => {
+  //     event.preventDefault();
+  //     const targetAdd = $(event.currentTarget);
+  //     targetAdd.find('#shopping-list-entry');
+  //   })));
+  // })
+  console.log('what?');
+
+  });
 }
 
 $(main());
